975176e71b8e7adb5358bf1b6022d8bbd069ab84
Quang Nguyen Dang <quangnguyendang@Quangs-MacBook-Pro.local>
2021-09-06 12:26:28 +0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `dqnotes`,
+    author: `Quang Nguyen`,
+    description: `Just a bunch of random thoughts`,
+    siteUrl: `https:dqnotes.com/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `itsquang`,
     },
   },
   plugins: [
