34cf373102b7d72ffa74f76e64c5bab5d3d72090
Quang Nguyen Dang <quangnguyendang@Quangs-MacBook-Pro.local>
2021-09-06 12:50:09 +0700

    update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,17 +17,15 @@ class IndexPage extends React.Component {
         />
         <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
-          Hey people{" "}
+          Hey there{" "}
           <span role="img" aria-label="wave emoji">
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
+        <p>I'm Quang. I publish my notes on this site.</p>
         <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
+          Not much yet since I've scrapped the old site. Yeah, old habit. I love writing in English, but I'm learning to write better in my mother tongue, Tiếng Việt, too.
         </p>
-        <p>Now go build something great!</p>
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
